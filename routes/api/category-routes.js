5752b64b40bb31f624b4abe086a595102852945a
Zakk Fast <zakkfastpro@gmail.com>
2021-09-12 19:43:21 -0700

    finished get all routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,18 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+Category.findAll(
+  {
+    include: {
+      model: Product,
+      attributes: ['product_name']
+    }
+  }
+).then(catData => res.json(catData))
+.catch(err => {
+  console.log(err)
+  res.status(500).json(err)
+})
 });
 
 router.get('/:id', (req, res) => {
