7dda70e1ddce786006cb5fe88eaca60505d4ee44
Zakk Fast <zakkfastpro@gmail.com>
2021-09-12 21:48:29 -0700

    added get by id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,6 @@ router.get("/", async (req, res) => {
     const tagData = await Tag.findAll({
       include: {
         model: Product,
-        attributes: ["product_name"],
       },
     });
     res.status(200).json(tagData);
@@ -17,9 +16,19 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "Nothing found with this id." });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
