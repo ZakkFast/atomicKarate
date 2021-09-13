4f3f81893d32f9633e76512623a6c7c99a53186f
Zakk Fast <zakkfastpro@gmail.com>
2021-09-12 20:28:17 -0700

    formatted code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,6 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
       include: {
@@ -34,8 +32,19 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "You need a name for the category!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
