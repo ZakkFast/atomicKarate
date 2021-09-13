d6a07d93c30853aaf66675f05d7e89489d115df3
Zakk Fast <zakkfastpro@gmail.com>
2021-09-12 20:16:27 -0700

    finished route for id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,39 +1,48 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-Category.findAll(
-  {
-    include: {
-      model: Product,
-      attributes: ['product_name']
-    }
+  try {
+    const categoryData = await Category.findAll({
+      include: {
+        model: Product,
+        attributes: ["product_name"],
+      },
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
   }
-).then(catData => res.json(catData))
-.catch(err => {
-  console.log(err)
-  res.status(500).json(err)
-})
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "Nothing found with this id." });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
